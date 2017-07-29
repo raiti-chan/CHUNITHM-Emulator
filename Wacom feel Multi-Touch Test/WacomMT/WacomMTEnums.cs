fbb81edacd73a8f1e795b85c31efce454cff5380
Raiti <rusuke@t-net.ne.jp>
2017-07-29 11:56:54 +0900

    InitialCommit

---
+++ Wacom feel Multi-Touch Test/WacomMT/WacomMTEnums.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Wacom_feel_Multi_Touch_Test.WacomMT.WacomMTEnums {
+
+	/// <summary>
+	/// エラー
+	/// </summary>
+	public enum WacomMTError : int {
+		/// <summary>
+		/// 呼び出しが成功した場合に返されます。
+		/// </summary>
+		WMTErrorSuccess = 0,
+		/// <summary>
+		/// インストール済みで実行中のドライバがAPIで検出されない場合に返されます。
+		/// </summary>
+		WMTErrorDriverNotFound = 1,
+		/// <summary>
+		/// ドライバのバージョンがAPIと互換性がない場合に返されます。
+		/// これは、アプリケーションがドライバでサポートされていない新しいAPIデータ構造体を要求する場合に発生します。
+		/// </summary>
+		WMTErrorBadVersion = 2,
+		/// <summary>
+		/// アプリケーションが、ドライバでサポートされなくなったAPIデータ構造体を要求する場合に返されます。
+		/// </summary>
+		WMTErrorAPIOutdated = 3,
+		/// <summary>
+		/// 1つ以上のパラメータが無効の場合に返されます。
+		/// </summary>
+		WMTErrorInvalidParam = 4,
+		/// <summary>
+		/// API終了呼び出しが行われる場合、またはAPIが正常に初期化されなかった場合に、待機関数によって返されます。
+		/// </summary>
+		WMTErrorQuit = 5,
+		/// <summary>
+		/// 提供されたバッファーが小さすぎる場合に返されます。
+		/// </summary>
+		WMTErrorBufferTooSmall = 6
+	}
+
+	/// <summary>
+	/// センサーデバイスの種類。<see cref="WacomMTCapability"/>構造体内で使用されます。
+	/// </summary>
+	public enum WacomMTDeviceType : int {
+		/// <summary>
+		/// タッチセンサーはディスプレイに組み込まれません。液晶表示のないトラックパッドのようなデバイスが、この値を返します。
+		/// </summary>
+		WMTDeviceTypeOpaque = 0,
+		/// <summary>
+		/// タッチセンサーがディスプレイに組み込まれます。タブレットPCなどのオンスクリーンタッチデバイスが、この値を返します。
+		/// </summary>
+		WMTDeviceTypeIntegrated = 1
+	}
+
+	/// <summary>
+	/// デバイスでサポートされるデータの種類を示すために、ケイパビリティー構造体内で使用されます。
+	/// </summary>
+	public enum WacomMTCapabilityFlags : int {
+		/// <summary>
+		/// このフラグが設定される場合、デバイスはRAWデータをサポートし、RAWデータの読み取りが可能になります。
+		/// </summary>
+		WMTCapabilityFlagsRawAvailable = (1 << 0),
+		/// <summary>
+		/// このフラグが設定される場合、デバイスはBLOBデータをサポートし、BLOBデータの読み取りが可能になります。
+		/// </summary>
+		WMTCapabilityFlagsBlobAvailable = (1 << 1),
+		/// <summary>
+		/// このフラグが設定される場合、フィンガーデータで感度データが使用可能になります。
+		/// 感度データは、0から0xFFFFまでの値です。
+		/// このフラグが設定されない場合、感度データは各アップパケットでゼロに設定され、ダウンパケット/ホールドパケットで最大に設定されます。
+		/// 感度の定義については、WacomMTFingerデータ構造体を参照してください。
+		/// </summary>
+		WMTCapabilityFlagsSensitivityAvailable = (1 << 2),
+		/// <summary>
+		/// ?
+		/// </summary>
+		WMTCapabilityFlagsReserved = (1 << 31)
+	}
+
+	/// <summary>
+	/// 指の状態を示すために、<see cref="WacomMTFinger"/>構造体内で使用されます。
+	/// </summary>
+	public enum WacomMTFingerState : int {
+		/// <summary>
+		/// 指バッファーには、複数の接触のスペースが含まれる可能性があります。
+		/// 使用されていない追加の接触は「None」に設定されます。
+		/// 接触が「アップ」状態を通過すると、TouchStateが「None」に設定され、これ以上の処理は不要であることが示されます。
+		/// この状態で組み込まれているデータがあれば、そのデータは無効です。
+		/// </summary>
+		WMTFingerStateNone = 0,
+		/// <summary>
+		/// 最初の指の接触を示します。特定の接触用の最初のタッチパケット。
+		/// </summary>
+		WMTFingerStateDown = 1,
+		/// <summary>
+		/// 指が接触している間の後続のパケット。
+		/// </summary>
+		WMTFingerStateHold = 2,
+		/// <summary>
+		/// 特定の指の接触用の最後のタッチパケット。指を上げるときに報告されます。これは、認識された最後の有効な位置で報告されます。
+		/// </summary>
+		WMTFingerStateUp = 3
+	}
+
+	/// <summary>
+	/// BLOBの種類を識別するために、BLOB構造体によって使用されます。
+	/// </summary>
+	public enum WacomMTBlobType : int {
+		/// <summary>
+		/// これは外側の基本BLOBの輪郭です。
+		/// </summary>
+		WMTBlobTypePrimary = 0,
+		/// <summary>
+		/// 外側の基本BLOB内に含まれている内側のBLOBの輪郭です。
+		/// ボイド BLOBは、基本BLOB内のタッチされていない領域です。基本 BLOBには、1つ以上のボイドBLOBが含まれている場合があります。
+		/// ボイド BLOBには、他のBLOBは含まれていません。固有の基本BLOBが別のBLOBのボイド内に存在することは可能ですが、ボイドはそのBLOBを参照しません。
+		/// </summary>
+		WMTBlobTypeVoid = 1
+	}
+
+	/// <summary>
+	/// コールバック関数によって使用されます。データの処理方法の指示を与えます。
+	/// フラグが設定されていない場合は、データがコールバックに送信され、システムによって処理されません。
+	/// </summary>
+	public enum WacomMTProcessingMode {
+		/// <summary>
+		/// データがコールバックに送信され、これ以上の処理は行われません。
+		/// </summary>
+		WMTProcessingModeNone = 0,
+		/// <summary>
+		/// データが処理のためにOSに並行して送られます。
+		/// </summary>
+		WMTProcessingModeObserver = (1 << 0),
+		/// <summary>
+		/// ?
+		/// </summary>
+		WMTProcessingModeReserved = (1 << 31)
+	}
+
+	/// <summary>
+	/// WindowMassage
+	/// </summary>
+	public enum WMTWindowMassage : int {
+		WM_FINGERDATA = 0x6205,
+		WM_BLOBDATA = 0x6206,
+		WM_RAWDATA = 0x6207
+	}
+}
---
+++ Wacom feel Multi-Touch Test/WacomMultiTouchTestMain.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+using Wacom_feel_Multi_Touch_Test.WacomMT;
+using Wacom_feel_Multi_Touch_Test.WacomMT.WacomMTEnums;
+using Wacom_feel_Multi_Touch_Test.WacomMT.WacomMTStructures;
+
+using static System.Console;
+
+namespace Wacom_feel_Multi_Touch_Test {
+	class WacomMultiTouchTestMain {
+
+		/// <summary>
+		/// デバイスのIDの配列
+		/// </summary>
+		private static int[] _Ids = null;
+
+		/// <summary>
+		/// メインメソッド
+		/// </summary>
+		/// <param name="args">プログラム引数</param>
+		[STAThread]
+		static void Main(string[] args) {
+			CursorVisible = false;
+			try {
+				WacomMTError error = WacomMTAPI.WacomMTInitialize();
+
+				if (error == WacomMTError.WMTErrorSuccess) {
+
+					_Ids = WacomMTAPI.WacomMTGetAttachedDeviceIDs();
+
+					foreach (int id in _Ids) {
+						WriteLine("Tablet Device #" + id);
+						WacomMTCapability capability = WacomMTAPI.WacomMTGetDeviceCapabilities(id);
+						WriteLine("Virsion : " + capability.Version);
+						WriteLine("DeviceID : " + capability.DeviceID);
+						WriteLine("Type : " + capability.Type);
+						WriteLine("LogicalOriginX : " + capability.LogicalOriginX);
+						WriteLine("LogicalOriginY : " + capability.LogicalOriginY);
+						WriteLine("LogicalWidth : " + capability.LogicalWidth);
+						WriteLine("LogicalHeight : " + capability.LogicalHeight);
+						WriteLine("PhysicalSizeX : " + capability.PhysicalSizeX);
+						WriteLine("PhysicalSizeY : " + capability.PhysicalSizeY);
+						WriteLine("ReportedSizeX : " + capability.ReportedSizeX);
+						WriteLine("ReportedSizeY : " + capability.ReportedSizeY);
+						WriteLine("ScanSizeX : " + capability.ScanSizeX);
+						WriteLine("ScanSizeY : " + capability.ScanSizeY);
+						WriteLine("FingerMax : " + capability.FingerMax);
+						WriteLine("BlobMax : " + capability.BlobMax);
+						WriteLine("BlobPointsMax : " + capability.BlobPointsMax);
+						WriteLine("CapabilityFlags : " + capability.CapabilityFlags);
+						WriteLine();
+					}
+
+					error = WacomMTAPI.WacomMTRegisterFingerReadCallback(_Ids[0], IntPtr.Zero, WacomMTProcessingMode.WMTProcessingModeNone, FingerCallback, IntPtr.Zero);
+
+					if (error != WacomMTError.WMTErrorSuccess) throw new Exception("Errpr WacomMTRegisterFingerReadCallback : " + error);
+
+				} else {
+					throw new Exception("Error WacomMTInitialize : " + error);
+				}
+			} catch (Exception e) {
+				SetCursorPosition(0, CursorTop + 2);
+				WriteLine(e.Message);
+			}
+
+			using (GrandSlider slider = new GrandSlider()) {
+				slider.Run(60);
+			}
+
+			/**
+			do {
+				Thread.Sleep(10);
+			} while (!Keyboard.IsKeyDown(Key.Enter));
+			*/
+				WacomMTAPI.WacomMTQuit();
+		}
+
+		public static UInt16 TouchState = 0;
+
+		public static UInt16 HoldState = 0;
+
+		private static int FingerCallback(IntPtr fingerPacket, IntPtr userData) {
+			WacomMTFingerCollectionClass fingerCollection = new WacomMTFingerCollectionClass(fingerPacket);
+			/*
+			WriteLine("DeviceID : " + fingerCollection.DeviceID);
+			WriteLine("-Version : " + fingerCollection.Version);
+			WriteLine("-FrameNumber : " + fingerCollection.FrameNumber);
+			WriteLine("-FingerCount : " + fingerCollection.FingerCount);
+			*/
+
+			TouchState = 0;
+			foreach (WacomMTFinger finger in fingerCollection.FingerData) {
+				/*
+				WriteLine("-FingerID : " + finger.FingerID);
+				WriteLine("--X : " + finger.X);
+				WriteLine("--Y : " + finger.Y);
+				WriteLine("--Width : " + finger.Width);
+				WriteLine("--Hight : " + finger.Height);
+				WriteLine("--Sensitivity : " + finger.Sensitivity);
+				WriteLine("--Orientation : " + finger.Orientation);
+				WriteLine("--Confidence : " + finger.Confidence);
+				WriteLine("--TouchState : " + finger.TouchState);
+				WriteLine();
+				*/
+				int underPanelIndex = (int)((finger.X - (finger.Width / 2)) * 16);
+				int centerpanelIndex = (int)(finger.X * 16);
+				int topPanelIndexs = (int)((finger.X + (finger.Width / 2)) * 16);
+
+				switch(finger.TouchState) {
+					case WacomMTFingerState.WMTFingerStateDown:
+						for (int i = underPanelIndex; i <= topPanelIndexs; i++) TouchState = (UInt16)(TouchState | (1 << i));
+						//TouchState = (UInt16)(TouchState | (1 << panelIndex));
+						break;
+					case WacomMTFingerState.WMTFingerStateHold:
+						for (int i = underPanelIndex; i <= topPanelIndexs; i++) TouchState = (UInt16)(TouchState | (1 << i));
+						//TouchState = (UInt16)(TouchState | (1 << panelIndex));
+						break;
+					case WacomMTFingerState.WMTFingerStateUp:
+						break;
+				}
+
+			}
+			
+			char[] chars = Convert.ToString(TouchState, 2).ToCharArray();
+			Array.Reverse(chars);
+			string text = new string(chars);
+			for (int i = text.Length; i < 16; i++) text += "0";
+			string text2 = "";
+			foreach (char c in text.ToCharArray()) text2 += (c == '0' ? "□" : "■");
+			WriteLine(text2);
+			
+			//WriteLine();
+			return 0;
+		}
+
+	}
+	
+	class GrandSlider : GameWindow {
+
+		public GrandSlider() : base (800,100,GraphicsMode.Default, "GrandSlider") {
+
+		}
+
+		protected override void OnLoad(EventArgs e) {
+			base.OnLoad(e);
+			GL.ClearColor(Color4.Black);
+		}
+
+		protected override void OnResize(EventArgs e) {
+			base.OnResize(e);
+
+			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+			GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+			GL.MatrixMode(MatrixMode.Projection);
+			Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / Height, 1.0f, 64.0f);
+			GL.LoadMatrix(ref projection);
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs e) {
+			base.OnRenderFrame(e);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.LoadIdentity();
+
+			Matrix4 view = Matrix4.CreateOrthographicOffCenter(0, 1024, 100, 0, 0, 10);
+			GL.MultMatrix(ref view);
+
+			GL.Disable(EnableCap.Normalize);
+			GL.Disable(EnableCap.Lighting);
+			GL.Disable(EnableCap.CullFace);
+			GL.Disable(EnableCap.DepthTest);
+
+			for (int i = 0; i < 16; i++) {
+
+				GL.Color4((WacomMultiTouchTestMain.TouchState & (1 << i)) > 0 ? Color4.Pink : Color4.Orange);
+				GL.Begin(PrimitiveType.Quads);
+				GL.Vertex2(64 * i, 0);
+				GL.Vertex2(64 * i, 100);
+				GL.Vertex2(64 * (i + 1), 100);
+				GL.Vertex2(64 * (i + 1), 0);
+				GL.End();
+				GL.Color4(Color4.White);
+				GL.Begin(PrimitiveType.Lines);
+				GL.Vertex2(64 * (i + 1), 0);
+				GL.Vertex2(64 * (i + 1), 100);
+				GL.End();
+			}
+
+			GL.Enable(EnableCap.DepthTest);
+			GL.Enable(EnableCap.CullFace);
+			GL.Enable(EnableCap.Lighting);
+			GL.Enable(EnableCap.Normalize);
+
+			GL.Flush();
+
+			SwapBuffers();
+			
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+}
